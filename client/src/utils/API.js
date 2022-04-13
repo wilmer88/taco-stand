e49188a4be32f0f3d836b9377d746c2eaca21166
Wilmer Rivera <wilmerbaby@gmail.com>
2022-04-13 14:34:37 -0400

    commited wrong axios routes

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,20 +3,20 @@ import axios from "axios";
 export default {
 
     saveOrden: function(ordenData) {
-        return axios.post("http://localhost:3001/api/orden", ordenData);
+        return axios.post("/api/orden", ordenData);
     },
     deleteOrden: function(id) {
-        return axios.delete("http://localhost:3001/api/orden/" + id)
+        return axios.delete("/api/orden/" + id)
     },
  
     getOrdens: function() {
-        return axios.get("http://localhost:3001/api/orden")
+        return axios.get("/api/orden")
     },
     getOrden: function(id) {
-        return axios.get("http://localhost:3001/api/orden/"+ id)
+        return axios.get("/api/orden/"+ id)
     },
   
     putOrden: function(id,ordenData) {
-        return axios.put("http://localhost:3001/api/orden/" + id, ordenData);
+        return axios.put("/api/orden/" + id, ordenData);
       }
 }
