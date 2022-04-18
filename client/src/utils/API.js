3687c8a6a67376eb4d7a56e7a486eb892ee73fbc
Wilmer Rivera <wilmerbaby@gmail.com>
2022-04-18 02:49:15 -0400

    heroku error testing deployment

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -3,25 +3,26 @@ import axios from "axios";
 export default {
 
     saveOrden: function(ordenData) {
-        return axios.post("http://localhost:3001/api/orden", ordenData);
+        return axios.post("/api/orden", ordenData);
     },
     deleteOrden: function(id) {
-        return axios.delete("http://localhost:3001/api/orden/" + id)
+        return axios.delete("h/api/orden/" + id)
     },
  
     getOrdens: function() {
-        return axios.get("http://localhost:3001/api/orden")
+        return axios.get("h/api/orden")
     },
     getOrden: function(id) {
-        return axios.get("http://localhost:3001/api/orden/"+ id)
+        return axios.get("/api/orden/"+ id)
     },
 
   
     putOrden: function(id,ordenData) {
-        return axios.put("http://localhost:3001/api/orden/" + id, ordenData);
+        return axios.put("/api/orden/" + id, ordenData);
       },
 
       saveUser: function(userData) {
-        return axios.post("http://localhost:3001/api/signup", userData);
+        return axios.post("/api/signup", userData);
     },
 }
+// "http://localhost:3001/api/orden"
