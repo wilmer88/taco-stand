1407377bf4e08c0f46276978151c0e9fab55b84e
Wilmer Rivera <wilmerbaby@gmail.com>
2022-04-18 19:21:14 -0400

    server.js mongoos connect params

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
+// const bodyParser = require("body-parser");
 require("dotenv").config();
 const routes= require("./routes")
 
@@ -18,8 +18,8 @@ mongoose
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-  //       useCreateIndex: true,
-  // useFindAndModify: false,
+        useCreateIndex: true,
+  useFindAndModify: false,
     }
   )
   .then(() => console.log("MongoDB has been connected"))
